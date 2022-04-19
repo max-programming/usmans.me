5037a8c8706fd95a5ee203583ca26512bb06e916
Max Programming <ui.sabuwala@gmail.com>
2022-04-19 06:11:32 +0530

    Some social links and animation

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,9 +1,14 @@
-import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import { Box, Flex, Image, Link, Text } from '@chakra-ui/react';
+import { motion, TargetAndTransition } from 'framer-motion';
+import { FaGithub, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 import { MyLink } from '@/layout/Link';
 import { SectionHeading } from '@/layout/SectionHeading';
 
 const Hero = () => {
+  const linkHover: TargetAndTransition = {
+    scale: 1.2,
+  };
   return (
     <Box mt="16">
       <Flex
@@ -28,6 +33,41 @@ const Hero = () => {
             </MyLink>{' '}
             on the subject of programming and mostly web development.
           </Text>
+          <SectionHeading display="flex" alignItems="center" gap={3}>
+            Catch me at:
+            <Flex gap={5}>
+              <Link
+                href="https://github.com/max-programming"
+                target="_blank"
+                as={motion.a}
+                initial={{ translateY: 1000 }}
+                animate={{ translateY: 0 }}
+                whileHover={linkHover}
+              >
+                <FaGithub />
+              </Link>
+              <Link
+                href="https://youtube.com/max-programming"
+                target="_blank"
+                as={motion.a}
+                initial={{ translateY: 1000 }}
+                animate={{ translateY: 0 }}
+                whileHover={linkHover}
+              >
+                <FaYoutube color="#FF0000" />
+              </Link>
+              <Link
+                href="https://twitter.com/MaxProgramming1"
+                target="_blank"
+                as={motion.a}
+                initial={{ translateY: 1000 }}
+                animate={{ translateY: 0 }}
+                whileHover={linkHover}
+              >
+                <FaTwitter color="#1DA1F2" />
+              </Link>
+            </Flex>
+          </SectionHeading>
         </Flex>
         <Flex>
           <Image src="/profilePic.jpg" alt="Profile Pic" rounded="full" />

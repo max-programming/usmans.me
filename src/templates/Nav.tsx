04674c19234accb09bff35f02f1bef43c9dd8639
Max Programming <ui.sabuwala@gmail.com>
2022-04-19 05:31:35 +0530

    Remove some links rename some

--- src/templates/Nav.tsx
+++ src/templates/Nav.tsx
@@ -9,19 +9,15 @@ const Nav = () => {
     <Box w="100%" bgGradient="linear(to-l, #114357, #F29492)">
       <Box p="5">
         <Flex align="center" justify={{ base: 'center', md: 'space-between' }}>
-          <Flex gap={5} hidden={isMobile}>
-            <MyLink href="/" fontSize="xl">
-              Home
-            </MyLink>
-            <MyLink href="/about" fontSize="xl">
-              About
-            </MyLink>
-          </Flex>
           <Heading textAlign="center" fontSize="4xl">
             Usman Sabuwala
           </Heading>
           <Flex gap={5} hidden={isMobile}>
-            <MyLink href="/blog" fontSize="xl">
+            <MyLink
+              href="https://blog.usman-s.me"
+              target="_blank"
+              fontSize="xl"
+            >
               Blog
             </MyLink>
             <MyLink href="/contact" fontSize="xl">

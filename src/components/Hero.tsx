f0e00308ac5af0ec9550965cc68d123917f473b3
Max Programming <ui.sabuwala@gmail.com>
2022-05-29 17:26:17 +0530

    💫 Added new and better animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import { Box, Flex, Image, SlideFade, Text } from '@chakra-ui/react';
 
 import { MyLink } from '@/components/Link';
 import { SectionHeading } from '@/components/SectionHeading';
@@ -15,43 +15,47 @@ const Hero = () => {
         justify="center"
         direction={{ base: 'column-reverse', md: 'row' }}
       >
-        <Flex direction="column" gap={5}>
-          <SectionHeading>
-            Hi, I am{' '}
-            <Text as="span" color="gradLeft">
-              Usman
+        <SlideFade offsetX={-1000} in transition={{ enter: { duration: 3 } }}>
+          <Flex direction="column" gap={5}>
+            <SectionHeading>
+              Hi, I am{' '}
+              <Text as="span" color="gradLeft">
+                Usman
+              </Text>
+              !
+            </SectionHeading>
+            <Text fontSize="xl">
+              Hey all! My name is Usman! I am 17 years old and learning web
+              development and programming. I also have a{' '}
+              <MyLink color="red.300" href="/youtube">
+                YouTube channel
+              </MyLink>{' '}
+              on the subject of programming and mostly web development.
             </Text>
-            !
-          </SectionHeading>
-          <Text fontSize="xl">
-            Hey all! My name is Usman! I am 17 years old and learning web
-            development and programming. I also have a{' '}
-            <MyLink color="red.300" href="/youtube">
-              YouTube channel
-            </MyLink>{' '}
-            on the subject of programming and mostly web development.
-          </Text>
-          <SectionHeading display="flex" alignItems="center" gap={3}>
-            Catch me at:
-            <Flex gap={5}>
-              {socialLinks.map((socialLink) => (
-                <SocialLink
-                  key={socialLink.link}
-                  link={socialLink.link}
-                  icon={socialLink.icon}
-                />
-              ))}
-            </Flex>
-          </SectionHeading>
-        </Flex>
-        <Flex>
-          <Image
-            src="/profilePic.jpg"
-            alt="Profile Pic"
-            rounded="full"
-            w={['xs', 'xs', 'xl', 'xl']}
-          />
-        </Flex>
+            <SectionHeading display="flex" alignItems="center" gap={3}>
+              Catch me at:
+              <Flex gap={5}>
+                {socialLinks.map((socialLink) => (
+                  <SocialLink
+                    key={socialLink.link}
+                    link={socialLink.link}
+                    icon={socialLink.icon}
+                  />
+                ))}
+              </Flex>
+            </SectionHeading>
+          </Flex>
+        </SlideFade>
+        <SlideFade in offsetX={1000} transition={{ enter: { duration: 1 } }}>
+          <Flex>
+            <Image
+              src="/profilePic.jpg"
+              alt="Profile Pic"
+              rounded="full"
+              w={['xs', 'xs', 'xl', 'xl']}
+            />
+          </Flex>
+        </SlideFade>
       </Flex>
     </Box>
   );

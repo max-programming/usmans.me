4e63d847a8c8f633c6587f16840f3ccb93b46d60
Max Programming <ui.sabuwala@gmail.com>
2022-05-29 17:31:23 +0530

    Few animation changes and responsive text

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -2,7 +2,12 @@ import { Heading, HeadingProps } from '@chakra-ui/react';
 
 const SectionHeading = (props: HeadingProps) => {
   return (
-    <Heading fontWeight="semibold" fontFamily="Sen" {...props}>
+    <Heading
+      fontWeight="semibold"
+      fontFamily="Sen"
+      fontSize={{ base: '2xl', md: '4xl' }}
+      {...props}
+    >
       {props.children}
     </Heading>
   );

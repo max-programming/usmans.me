538c9f507e30cb1423b9e72ea2bec6237f22e73e
Max Programming <ui.sabuwala@gmail.com>
2022-12-22 21:43:21 +0530

    Display comments

--- src/components/PostsHome.tsx
+++ src/components/PostsHome.tsx
@@ -1,7 +1,7 @@
 import { Box, Flex, Image, Link, Stack, Text } from '@chakra-ui/react';
 import { Player } from '@lottiefiles/react-lottie-player';
 import { motion } from 'framer-motion';
-import { FaThumbsUp } from 'react-icons/fa';
+import { FaThumbsUp, FaComment } from 'react-icons/fa';
 
 import { SectionHeading } from '@/components/SectionHeading';
 import { Post } from '@/utils/fetchPosts';
@@ -21,41 +21,51 @@ const PostCard = ({ post, isEven }: { post: Post; isEven: boolean }) => (
   >
     <Link
       href={`https://blog.usmans.me/${post.slug}`}
-      target="_blank"
+      target='_blank'
       _hover={{ textDecoration: 'none' }}
       _focus={{ boxShadow: 'none' }}
     >
       <Flex
         gap={10}
-        justify="center"
-        rounded="md"
-        align="center"
-        p="4"
+        justify='center'
+        rounded='md'
+        align='center'
+        p='4'
         direction={{
           base: 'column',
           md: isEven ? 'row' : 'row-reverse',
         }}
       >
-        <Image src={post.coverImage} alt={post.title} rounded="md" w="xs" />
+        <Image src={post.coverImage} alt={post.title} rounded='md' w='xs' />
         <Stack>
-          <SectionHeading size="lg">{post.title}</SectionHeading>
+          <SectionHeading size='lg'>{post.title}</SectionHeading>
           <Flex gap={5}>
             <Text
-              fontSize="xl"
+              fontSize='xl'
               title={`${post.totalReactions} Reactions`}
-              display="flex"
+              display='flex'
               gap={1}
-              alignItems="center"
+              alignItems='center'
             >
               <FaThumbsUp />
               {post.totalReactions}
             </Text>
             <Text
-              fontSize="xl"
-              title={`Published on ${new Date(post.dateAdded).toDateString()}`}
-              display="flex"
+              fontSize='xl'
+              title={`${post.responseCount + post.replyCount} Comments`}
+              display='flex'
               gap={1}
-              alignItems="center"
+              alignItems='center'
+            >
+              <FaComment />
+              {post.responseCount + post.replyCount}
+            </Text>
+            <Text
+              fontSize='xl'
+              title={`Published on ${new Date(post.dateAdded).toDateString()}`}
+              display='flex'
+              gap={1}
+              alignItems='center'
             >
               {new Date(post.dateAdded).toDateString()} <br />
             </Text>
@@ -74,13 +84,13 @@ const PostsHome = ({ posts }: IPostsHomeProps) => {
           as={Player}
           loop
           autoplay
-          src="lotties/point_down.json"
+          src='lotties/point_down.json'
           w={{ base: '10', md: '16' }}
           h={{ base: '10', md: '16' }}
         />
         <SectionHeading>My recent blog posts</SectionHeading>
       </Flex>
-      <Flex gap={5} direction="column" mt="5">
+      <Flex gap={5} direction='column' mt='5'>
         {posts.map((post, index) => (
           <PostCard key={post.cuid} post={post} isEven={index % 2 === 0} />
         ))}

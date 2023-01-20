05588a6c268348b24d50ea3cb4a3d22e8349abb6
Max Programming <ui.sabuwala@gmail.com>
2023-01-21 01:12:48 +0530

    YouTube videos display. Closes #2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from '@chakra-ui/react';
+import { Box, Container, Text, Grid, Flex } from '@chakra-ui/react';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import useSWR from 'swr';
 
@@ -6,21 +6,27 @@ import { Hero } from '@/components/Hero';
 import { Main } from '@/components/Main';
 import { Meta } from '@/components/Meta';
 import { Nav } from '@/components/Nav';
+import YouTubeCard from '@/components/YouTubeCard';
 import { PostsHome } from '@/components/PostsHome';
 import { SectionHeading } from '@/components/SectionHeading';
 import { getPosts, LikesAndComments, Post } from '@/utils/fetchPosts';
 import { getVideos, Video } from '@/utils/fetchVideos';
+import { Player } from '@lottiefiles/react-lottie-player';
 
 interface Props {
   posts: Post[];
   videos: Video[];
 }
 
-const Index = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Index = ({
+  posts,
+  videos,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { data: likesAndComments } = useSWR<LikesAndComments[]>(
     '/api/likesAndComments',
     url => fetch(url).then(res => res.json())
   );
+
   return (
     <Main
       meta={
@@ -40,9 +46,37 @@ const Index = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
           mx='auto'
           bgColor='whiteAlpha.500'
         />
-        <SectionHeading size='2xl' my='4'>
+        <SectionHeading size='2xl' my='2'>
           What do I do?
         </SectionHeading>
+        <Flex align='center'>
+          <Text
+            as={Player}
+            loop
+            autoplay
+            src='/lotties/youtube.json'
+            w={{ base: '20', md: '20' }}
+            h={{ base: '20', md: '20' }}
+          />
+          <SectionHeading>My latest YouTube videos</SectionHeading>
+        </Flex>
+        <Grid
+          templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }}
+          gap={6}
+          mt='4'
+          mb='10'
+          placeItems='center'
+        >
+          {videos.map(video => (
+            <YouTubeCard
+              thumbnail={video.thumbnails.medium.url}
+              title={video.title}
+              key={video.id}
+              id={video.id}
+              stats={video.stats}
+            />
+          ))}
+        </Grid>
         <PostsHome posts={posts} likesAndComments={likesAndComments} />
       </Container>
     </Main>
@@ -54,7 +88,7 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
   const allPosts = await getPosts();
   const videos = await getVideos();
 
-  const posts = allPosts.slice(0, 4);
+  const posts = allPosts.slice(0, 6);
 
   return {
     props: {

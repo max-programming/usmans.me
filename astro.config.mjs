455e646b9990b82c76981b2fbceee45008f2795b
Max Programming <ui.sabuwala@gmail.com>
2023-02-23 01:00:47 +0530

    Add Astro Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
-// https://astro.build/config
 import vercel from '@astrojs/vercel/serverless';
-
-// https://astro.build/config
 import image from '@astrojs/image';
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()],
+  integrations: [tailwind(), react(), image(), sitemap()],
   output: 'server',
   adapter: vercel(),
   site: 'https://usmans.me',
-  vite: {
-    plugins: [viteCommonjs()],
-  },
 });

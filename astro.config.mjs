f7d5d679cb4b839a7b2d2b6ee814560e3b9db0b6
Max Programming <ui.sabuwala@gmail.com>
2023-02-22 21:27:40 +0530

    Change site property in Astro Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), image()],
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel(),
+  site: 'https://usmans.me',
 });

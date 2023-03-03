12e5f70a681ab6bc8092a6ec6f353607fa7cd33a
Max Programming <ui.sabuwala@gmail.com>
2023-03-03 17:25:46 +0530

    Error handling discord message

--- src/pages/api/sendDiscordMessage.ts
+++ src/pages/api/sendDiscordMessage.ts
@@ -1,18 +1,23 @@
 import type { APIRoute } from 'astro';
 
 export const post: APIRoute = async ({ params, request }) => {
-  const body = await request.json();
-  const name = body.name;
-  const response = await fetch(import.meta.env.DISCORD_WEBHOOK_URL, {
-    method: 'POST',
-    body: JSON.stringify({
-      content: `**${name}** button was clicked.`,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  return new Response(JSON.stringify(response), {
-    status: 200,
-  });
+  try {
+    const body = await request.json();
+    const name = body.name;
+    const response = await fetch(import.meta.env.DISCORD_WEBHOOK_URL, {
+      method: 'POST',
+      body: JSON.stringify({
+        content: `**${name}** button was clicked.`,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return new Response(JSON.stringify(response), {
+      status: 200,
+    });
+  } catch (e) {
+    console.log({ error: e });
+    return new Response(null, { status: 500 });
+  }
 };

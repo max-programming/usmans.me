0b320a09d729b64c7235cdf808ee3ddc3f548661
Max Programming <ui.sabuwala@gmail.com>
2023-03-03 17:08:12 +0530

    Remove content type check

--- src/pages/api/sendDiscordMessage.ts
+++ src/pages/api/sendDiscordMessage.ts
@@ -1,21 +1,18 @@
 import type { APIRoute } from 'astro';
 
 export const post: APIRoute = async ({ params, request }) => {
-  if (request.headers.get('Content-Type') === 'application/json') {
-    const body = await request.json();
-    const name = body.name;
-    const response = await fetch(import.meta.env.DISCORD_WEBHOOK_URL, {
-      method: 'POST',
-      body: JSON.stringify({
-        content: `**${name}** button was clicked.`,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    return new Response(JSON.stringify(response), {
-      status: 200,
-    });
-  }
-  return new Response(null, { status: 400 });
+  const body = await request.json();
+  const name = body.name;
+  const response = await fetch(import.meta.env.DISCORD_WEBHOOK_URL, {
+    method: 'POST',
+    body: JSON.stringify({
+      content: `**${name}** button was clicked.`,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return new Response(JSON.stringify(response), {
+    status: 200,
+  });
 };

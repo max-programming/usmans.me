0b3dbde2019ed53dd2c9b7efa9d176edd12ad7be
Max Programming <ui.sabuwala@gmail.com>
2023-03-06 00:08:35 +0530

    Hamburger menu and hover effects are back

---
+++ src/components/Hamburger.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import useMediaQuery from '../utils/useMediaQuery';
+
+export default function Hamburger() {
+  const isMobile = useMediaQuery('(max-width: 768px)');
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    isMobile && (
+      <div className='container flex flex-wrap items-center justify-between mx-auto'>
+        <div className='absolute right-5 top-5'>
+          <button
+            type='button'
+            onClick={() => setIsOpen(!isOpen)}
+            className='inline-flex items-center p-2 ml-3 text-sm rounded-lg  focus:outline-none focus:ring-2 text-primary-light hover:bg-primary-darker focus:ring-gray-600'
+          >
+            <span className='sr-only'>Open main menu</span>
+            <svg
+              className='w-6 h-6'
+              aria-hidden='true'
+              fill='currentColor'
+              viewBox='0 0 20 20'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                fill-rule='evenodd'
+                d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
+                clip-rule='evenodd'
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div className={`${isOpen ? 'block' : 'hidden'} w-full`}>
+          <ul className='flex flex-col mt-4 rounded-lg bg-primary-dark'>
+            <li>
+              <a
+                href='/skills'
+                className='block py-2 pl-3 pr-4 text-white rounded active:text-primary-light active:bg-primary-darker'
+              >
+                Skills
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://blog.usmans.me'
+                className='block py-2 pl-3 pr-4 text-white rounded active:text-primary-light active:bg-primary-darker'
+              >
+                Blog
+              </a>
+            </li>
+            <li>
+              <a
+                href='/gists'
+                className='block py-2 pl-3 pr-4 text-white rounded active:text-primary-light active:bg-primary-darker'
+              >
+                Gists
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  );
+}

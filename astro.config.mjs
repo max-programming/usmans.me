6dda05339d304ff724d685fe923db3225f766e14
Max Programming <ui.sabuwala@gmail.com>
2023-03-08 22:57:51 +0530

    Some styling fix and switch to Vercel Edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/edge';
 import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 

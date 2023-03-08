bc30dcce3313d5155d8c412c15bd88006f84464c
Max Programming <ui.sabuwala@gmail.com>
2023-03-08 23:08:25 +0530

    Switch back to serverless Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config

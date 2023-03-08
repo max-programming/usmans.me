464262e7204f3e78647c28c80cb4e3c9f03a25bc
Max Programming <ui.sabuwala@gmail.com>
2023-03-08 23:03:32 +0530

    Remove Astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/edge';
-import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image(), sitemap()],
+  integrations: [tailwind(), react(), sitemap()],
   output: 'server',
   adapter: vercel(),
   site: 'https://usmans.me',

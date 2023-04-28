62388aec39ae1127d83eea5c3ec67664d6271e66
Max Programming <ui.sabuwala@gmail.com>
2023-04-28 23:07:43 +0530

    Add a Projects Page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.js',
+  ],
   theme: {
     fontFamily: {
       sans: ['Sen', 'sans-serif'],
@@ -22,5 +25,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };

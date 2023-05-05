d0d937a81e460edd72e4a346e8088d02cf32b2e6
Max Programming <ui.sabuwala@gmail.com>
2023-05-05 17:46:07 +0530

    remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap()],
   output: 'server',
-  adapter: vercel(),
   site: 'https://usmans.me',
   vite: {
     ssr: {

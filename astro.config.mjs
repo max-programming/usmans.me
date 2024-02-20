238aa8e95d7006b47ad76e29943f134c0be273ef
Usman S. (Max Programming) <ui.sabuwala@gmail.com>
2024-02-20 10:10:46 +0530

    Update dependencies, add proper config and ISR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 import million from 'million/compiler';
 
@@ -8,8 +9,19 @@ import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap(), robotsTxt()],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap(),
+    robotsTxt(),
+    vercel({
+      isr: {
+        expiration: 60 * 60 * 24,
+      },
+    }),
+  ],
   site: 'https://usmans.me',
+  output: 'hybrid',
   devToolbar: {
     enabled: true,
   },

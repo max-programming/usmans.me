9d2d02add3873d6de6825abfe088bede9b35a805
Usman S. <ui.sabuwala@gmail.com>
2025-09-02 17:00:11 +0000

    Update dependencies, fix type error and fix config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,35 @@
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import lottie from "astro-integration-lottie";
-import { defineConfig } from "astro/config";
-import million from "million/compiler";
-import robotsTxt from "astro-robots-txt";
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
+import lottie from 'astro-integration-lottie';
+import { defineConfig } from 'astro/config';
+import million from 'million/compiler';
+import robotsTxt from 'astro-robots-txt';
 
-import db from "@astrojs/db";
+import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: vercel(),
-	integrations: [tailwind(), react(), sitemap(), robotsTxt(), lottie(), db()],
-	site: "https://usmans.me",
-	output: "hybrid",
-	devToolbar: {
-		enabled: true,
-	},
-	vite: {
-		plugins: [
-			// million.vite({
-			//   mode: 'react',
-			//   server: true,
-			//   auto: true,
-			//   mute: true,
-			// }),
-		],
-		ssr: {
-			noExternal: ["usehooks-ts"],
-		},
-	},
+  adapter: vercel(),
+  integrations: [react(), sitemap(), robotsTxt(), lottie(), db()],
+  site: 'https://usmans.me',
+  output: 'static',
+  devToolbar: {
+    enabled: true,
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+      // million.vite({
+      //   mode: 'react',
+      //   server: true,
+      //   auto: true,
+      //   mute: true,
+      // }),
+    ],
+    ssr: {
+      noExternal: ['usehooks-ts'],
+    },
+  },
 });
--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -1,38 +1,42 @@
-import { type TargetAndTransition, motion } from "framer-motion";
-import type { ReactNode } from "react";
-import { socialLinks } from "../utils/socialLinks";
+import { motion } from 'framer-motion';
+import { socialLinks } from '../utils/socialLinks';
+import type { TargetAndTransition } from 'framer-motion';
 
 function SocialLinks() {
-	return (
-		<div className="flex gap-3 md:gap-5">
-			{socialLinks.map((socialLink) => (
-				<SocialLink
-					key={socialLink.link}
-					link={socialLink.link}
-					icon={socialLink.icon}
-					name={socialLink.name}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div className='flex gap-3 md:gap-5'>
+      {socialLinks.map(socialLink => (
+        <SocialLink
+          key={socialLink.link}
+          link={socialLink.link}
+          icon={socialLink.icon}
+          name={socialLink.name}
+        />
+      ))}
+    </div>
+  );
 }
 
-const SocialLink = (props: { link: string; icon: ReactNode; name: string }) => {
-	const linkHover: TargetAndTransition = {
-		scale: 1.2,
-	};
+const SocialLink = (props: {
+  link: string;
+  icon: JSX.Element;
+  name: string;
+}) => {
+  const linkHover: TargetAndTransition = {
+    scale: 1.2,
+  };
 
-	return (
-		<motion.a
-			whileHover={linkHover}
-			href={props.link}
-			target="_blank"
-			rel="noreferrer"
-			className="text-4xl"
-		>
-			{props.icon}
-		</motion.a>
-	);
+  return (
+    <motion.a
+      whileHover={linkHover}
+      href={props.link}
+      target='_blank'
+      rel='noreferrer'
+      className='text-4xl'
+    >
+      {props.icon}
+    </motion.a>
+  );
 };
 
 export { SocialLinks };
